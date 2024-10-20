af60fe41d892cdc59d874507b85ca8fd47ed2d50
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-10-20 17:49:22 +0200

    Log-in redirection implemented

--- app.js
+++ app.js
@@ -20,20 +20,50 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+			const user = rows[0];
+
+			if (!user)
+				return done(null, false, { message: "Incorrect username" });
+			if (user.password !== password)
+				return done(null, false, { message: "Incorrect password" });
+			return done(null, user);
+		} catch (error) {
+			return done(error);
+		}
+	})
+);
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+	try {
+		const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+		const user = rows[0];
+		done(null, user);
+	} catch (error) {
+		done(error);
+	}
+});
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("signUpForm"));
+app.get("/log-in", (req, res) => res.render("logInForm"));
 
 app.post("/sign-up", async (req, res, next) => {
 	try {
-	  await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-		req.body.username,
-		req.body.password,
-	  ]);
-	  res.redirect("/");
-	} catch(err) {
-	  return next(err);
+		await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+			req.body.username,
+			req.body.password,
+		]);
+		res.redirect("/");
+	} catch (err) {
+		return next(err);
 	}
-  });
+});
+app.post("/log-in", passport.authenticate("local", { successRedirect: "/", failureRedirect: "/" }));
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
