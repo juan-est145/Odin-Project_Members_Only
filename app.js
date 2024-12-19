8b2947ae71718e573c97479f4346d21db7a4303d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-19 11:31:25 +0100

    Added favicon

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "styles")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(sessionConf);
 app.use(passportConf);
 app.use((req, res, next) => {
