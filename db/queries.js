7bfa5189339642859df5a32df69b1a98ff54795b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-20 17:36:46 +0100

    We can see the user that made the message too

--- db/queries.js
+++ db/queries.js
@@ -13,6 +13,8 @@ async function getUser(user) {
 }
 
 async function getUserById(id) {
+	if (!id)
+		return (null);
 	try {
 		const { rows } = await pool.query("SELECT * FROM users where id = $1;", [id]);
 		return (rows);
@@ -22,6 +24,8 @@ async function getUserById(id) {
 }
 
 async function signUser(user, password) {
+	if (!user || !password)
+		return (null);
 	try {
 		await pool.query("INSERT INTO users(username, password) values($1, $2);", [user, password]);
 	} catch (error) {
@@ -31,7 +35,8 @@ async function signUser(user, password) {
 
 async function getAllMsgs() {
 	try {
-		const { rows } = await pool.query("SELECT * FROM message");
+		const { rows } = await pool.query("SELECT message.*, users.username FROM message \
+											INNER JOIN users on message.users_id=users.id;");
 		return (rows);
 	} catch (error) {
 		throw error;
