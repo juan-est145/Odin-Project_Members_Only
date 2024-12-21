d3e705a84ecb94741ed90448bb321c956da04ecc
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-21 14:58:13 +0100

    The messages have been styled

--- public/messages.js
+++ public/messages.js
@@ -1,5 +1,17 @@
 const modalBtn = document.getElementById("msgBtn");
+const modal = document.getElementById("postMessage");
 
 modalBtn.addEventListener("click", () => {
-	document.getElementById("postMessage").showModal();
+	modal.showModal();
+});
+
+modal.addEventListener("click", (e) => {
+	const modalDimension = modal.getBoundingClientRect();
+	if (
+		e.clientX < modalDimension.left ||
+		e.clientX > modalDimension.right ||
+		e.clientY < modalDimension.top ||
+		e.clientY > modalDimension.bottom
+	)
+		modal.close();
 });
